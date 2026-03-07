a5a3c7309f0aa150fd5e683c8b876105fc1b4da9
HarryS561 <2608771343@qq.com>
2026-03-07 00:37:45 -0800

    accessibility

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
-运行 find_duplicate.py
-检查重复 ID：修改其中一个的 image
-目前重复 ID 只有: LOST_WISP
-    relics: {'id': 'LOST_WISP', 'name': '迷失鬼火', 'pool': 'colorless', 'tier': 'Event', 'description': '你每打出一张能力牌，就对所有敌人造成8点伤害。', 'flavorText': '这个遗物的细节将在未来揭晓……', 'v': 2, 'mod': 'Slay the Spire 2'}
-    events: {'id': 'LOST_WISP', 'name': '迷失鬼火', 'description': '远处有一幕古怪的景象 。成堆的昆虫残骸层层堆叠，围绕着一个像是微小的光团的东西。\n它似乎对各种虫子都有着不俗的吸引力，但你却目睹了它迸发出火焰！\n\n你走近前去一探究竟。', 'options': ['搜索附近的区域', '获得60金币。', '已锁定', '你的最大生命值太低了。', '抓住这团鬼火', '将一张腐朽添加到你的牌组。获得迷失鬼火。'], 'v': 2, 'mod': 'Slay the Spire 2'}
+# 安装
 
-检查重复 name：检查对应页面
+使用前请从 `config.example.json` 复制一份到 `config.json`，然后在 `config.json` 中填入：
+* 灰机维基的机器人用户名、密码和机器人操作所需的 header（没有的话找 Revela255 要）
+
+记得安装依赖
+```bat
+pip install -r requirements.txt
+```
+
+# 使用
+各种`update_xxx.py`用途都很直观就不说了
+
+`find_duplicate.py`可以检查重复的 ID 和 name（中文名）
+如果存在重复 ID，则需要修改其中一个的 image。目前重复 ID 只有 LOST_WISP，既是遗物又是事件
+如果存在重复 name，则需要检查 wiki 的对应页面
--- update_image.py
+++ update_image.py
@@ -2,13 +2,16 @@ from utils import *
 
 base_path = 'C:/Program Files (x86)/Steam/steamapps/common/Slay the Spire 2/export/slay-the-spire-2'
 
-# card_images = os.path.join(base_path, 'card-images')
-# for filename in os.listdir(card_images):
-#     if filename.endswith('.png'):
-#         new_name = filename.lower().replace('plus1', '_upgrade')
-#         site.upload(os.path.join(card_images, filename), new_name, '[[分类:卡牌图像]]', True)
-
 exist = {image.name for image in site.allimages()}
+# 之后官方改卡图的话就得重新传一遍，不能有旧图就不传了
+
+card_images = os.path.join(base_path, 'card-images')
+for filename in os.listdir(card_images):
+    if filename.endswith('.png'):
+        new_name = filename.lower().replace('plus1', '_upgrade')
+        if not ("文件:" + new_name.capitalize().replace('_', ' ')) in exist:
+            site.upload(os.path.join(card_images, filename), new_name, '[[分类:卡牌图像]]', True)
+
 for en, cn in [('relics', '遗物'), ('potions', '药水'), ('enchantments', '附魔'), ('afflictions', '苦痛')]:
     images = os.path.join(base_path, en)
     for filename in tqdm(os.listdir(images)):
